b6306eed4e1e5f8004480d68c1807746a3abc952
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-03 11:31:06 -0800

    create data by task

--- create_data_custom.py
+++ create_data_custom.py
@@ -1,5 +1,6 @@
 import argparse
 import utils.data
+import os, json
 
 def main(args: dict) -> None:
     handler = getattr(utils.data, args.variant + "_handler")
@@ -9,7 +10,8 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--datasets", type=str, default=None)
+    parser.add_argument("--task", type=str, default=None)
     parser.add_argument("--k", type=int, default=16)
     parser.add_argument("--seed", type=str, default="100,13,21,42,87")
     parser.add_argument("--variant", type=str, required=True)
@@ -20,5 +22,11 @@ if __name__=='__main__':
     parser.add_argument("--corpus_path", type=str, default=None)
 
     args = parser.parse_args()
+    
+    assert args.datasets is not None or args.task is not None, "Either datasets or task should be provided"
+    if args.datasets is None:
+        with open(os.path.join("config", args.task + ".json"), "r") as f:
+            config = json.load(f)
+        args.datasets = config["train"] + config["test"] + config["unseen_domain_test"]
 
     main(args)
