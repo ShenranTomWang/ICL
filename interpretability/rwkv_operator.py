61085cab08d9cc506efe22d3b8c81ff8a50b76f3
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-19 13:01:46 -0800

    rwkv debugged

--- interpretability/rwkv_operator.py
+++ interpretability/rwkv_operator.py
@@ -21,36 +21,53 @@ class RWKVOperator(Operator):
             layers (list): list of layer indices
             activation_callback_k (function(tuple[torch.Tensor])): callback function for cache, applied to all cache from all layers
         Returns:
-            torch.Tensor: tensor of shape (n_inputs, n_layers, n_heads, head_dim, head_dim)
+            tuple[torch.Tensor]: (n_inputs, n_layers, hidden_size), (n_inputs, n_layers, n_heads, head_dim, head_dim), (n_inputs, n_layers, hidden_size)
         """
         def extract_single_line(input: str) -> torch.Tensor:
             tokenized = self.tokenizer(input, return_tensors="pt", truncation=True).to(self.device)
             cache = self.model(**tokenized, use_cache=True).state
-            kv = cache[1]
-            cache = torch.movedim(kv[..., layers], -1, 0).squeeze(1)     # (n_layers, n_heads, head_dim, head_dim)
-            cache = cache.cpu()
+            x, kv, ffn = cache[0], cache[1], cache[2]
+            x = torch.movedim(x[..., layers], -1, 0).squeeze(1).cpu()       # (n_layers, hidden_size)
+            kv = torch.movedim(kv[..., layers], -1, 0).squeeze(1).cpu()     # (n_layers, n_heads, head_dim, head_dim)
+            ffn = torch.movedim(ffn[..., layers], -1, 0).squeeze(1).cpu()   # (n_layers, hidden_size)
+            cache = (x, kv, ffn)
             cache = activation_callback(cache)
             return cache
         
-        caches = []
+        xs, kvs, ffns = [], [], []
         for input in inputs:
-            cache = extract_single_line(input)
-            caches.append(cache)
-        caches = torch.stack(caches, dim=0)
+            x, kv, ffn = extract_single_line(input)
+            xs.append(x)
+            kvs.append(kv)
+            ffns.append(ffn)
+        xs = torch.stack(xs, dim=0)
+        kvs = torch.stack(kvs, dim=0)
+        ffns = torch.stack(ffns, dim=0)
         
-        return caches
+        return xs, kvs, ffns
     
-    def store_cache(self, cache: torch.Tensor, path: str) -> None:
+    def store_cache(self, cache: tuple[torch.Tensor], path: str) -> None:
         """
         Store cache to path
         Args:
-            cache (torch.Tensor)
+            cache (tuple[torch.Tensor]): (n_inputs, n_layers, hidden_size), (n_inputs, n_layers, n_heads, head_dim, head_dim), (n_inputs, n_layers, hidden_size)
             path (str)
         """
         logger = logging.getLogger(__name__)
+        xs, kvs, ffns = cache
         if path.endswith(".pt"):
             path = path[:-3]
-        path = f"{path}_kv.pt"
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        torch.save(cache, path)
+            
+        out_path = f"{path}_x.pt"
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        torch.save(cache, out_path)
+        
+        out_path = f"{path}_kv.pt"
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        torch.save(cache, out_path)
+        
+        out_path = f"{path}_ffn.pt"
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        torch.save(cache, out_path)
+        
         logger.info(f"Saved activations to {path}")
