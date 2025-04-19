319908ddf1f55fc95b61e7b327f2c405ea9edcc2
shenranw <wsr.20021128@gmail.com>
2025-04-19 14:31:03 -0700

    create incorrectly mapped data

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 ALL_OPERATORS = ["Qwen2Operator", "HymbaOperator", "RWKVOperator", "MambaOperator", "Mamba2Operator", "ZambaOperator", "ForwardWrapperTransformerOperator", "HybridOperator", "LlamaOperator"]
 ALL_DTYPES = ["bfloat16", "float16", "float32"]
-ALL_VARIANTS = ["random", "0_correct", "25_correct", "50_correct", "75_correct", "random_english_words"]
+ALL_VARIANTS = ["random", "0_correct", "25_correct", "50_correct", "75_correct", "random_english_words", "incorrect_mapping"]
