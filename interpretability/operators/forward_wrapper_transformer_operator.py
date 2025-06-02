f696c57b119f9ca396bc055d4f946c4511dfece6
shenranw <wsr.20021128@gmail.com>
2025-06-01 21:12:05 -0700

    debug

--- interpretability/operators/forward_wrapper_transformer_operator.py
+++ interpretability/operators/forward_wrapper_transformer_operator.py
@@ -15,7 +15,8 @@ class ForwardWrapperTransformerOperator(TransformerOperator):
         tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
         model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True).to(device).to(dtype)
         n_layers = model.config.num_hidden_layers
-        super().__init__(model, Llama3Tokenizer(tokenizer), device, dtype, n_layers)
+        n_heads = model.config.num_attention_heads
+        super().__init__(model, Llama3Tokenizer(tokenizer), device, dtype, n_layers, n_heads)
         
     def generate_AIE_map(self, steer, inputs, label_ids) -> FVMap:
         raise NotImplementedError("ForwardWrapperTransformerOperator does not support AIE map generation")
